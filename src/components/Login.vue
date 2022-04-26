e215033c2715032b488ee2b5944ba10c887eacc7
Jacob <vb2553yq@go.minnstate.edu>
2022-04-25 22:26:21 -0500

    Added getTruck and Login function

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -20,7 +20,6 @@
 
 <script>
 import store from "@/store/index.js";
-import router from "@/router/index.js";
 
 export default {
   name: "loginForm",
@@ -42,13 +41,31 @@ export default {
     },
   },
   methods: {
-    login: function () {
-      store.state.auth = true;
-      store.state.username = this.username;
-
-      this.username = "";
-      this.password = "";
-      router.push({ path: "Trucks" });
+    login: function() {
+        const formdata = new FormData();
+        var vm = this;
+      
+        formdata.append("username",this.username);
+        formdata.append("password",this.password);
+      
+        fetch("http://localhost/src/php/login.php", {
+          method: "post",
+          body: formdata
+          })
+          .then(response => response.json())
+          .then(function(data) {
+            if(data.status == "Success") {
+                vm.username = "";
+                vm.password = "";
+            
+              store.commit("setAuth", {
+                    auth: data.auth,
+                    session: data.session
+                });
+              } else {
+                console.log(data);
+              }
+          });
     },
     logout: function () {
       store.state.auth = false;
@@ -57,3 +74,32 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+input[type=text], input[type=password] {
+    width: 100%;
+    padding: 15px;
+    margin: 5px 0 22px 0;
+    display: inline-block;
+    border: none;
+    background: #f1f1f1;
+}
+
+input[type=text]:focus {
+    background-color: #ddd;
+    outline: none;
+}
+
+button {
+    background-color: green;
+    border: none;
+    color: white;
+    padding: 10px 10px;
+    text-align: center;
+    text-decoration: none;
+    font-size: 14px;
+}
+
+* {box-sizing: border-box}
+
+</style>
--- src/php/getTrucks.php
+++ src/php/getTrucks.php
@@ -5,59 +5,36 @@ require_once "config.php";
 session_id($_POST['session']);
 session_start();
 
-if ($_SESSION['auth'])
-{
-    $connection = new mysqli($hn, $un, $pw, $db);
+$vehicles = [];
 
-    if ($connection->connect_error)
-    {
-        $response['outcome'] = "error";
-        $response['err-msg'] = "Could not connect to database";
-    }
-    else
-    {
-        $stmt = $connection->prepare("SELECT * FROM vehicles");
-        if ($stmt->execute())
-        {
-            $vehiclesResult = $stmt->get_result();
-            $numRows = $vehicles->num_rows;
+$connection = new mysqli($hn, $un, $pw, $db);
 
-            if ($numRows > 0)
-            {
+if ($connection->connect_error) {
+    $response['outcome'] = "error";
+    $response['err-msg'] = "Could not connect to database";
+} else {
+    $stmt = $connection->prepare("SELECT * FROM vehicles");
+    if ($stmt->execute()) {
+        $result = $stmt->get_result();
+        $rows = $result->num_rows;
+        if ($rows >= 1) {
+            foreach ($result as $vehicle) {
+                $vehicles[] = $vehicle;
                 $response['outcome'] = "success";
-                $vehicles = [];
-
-                foreach ($vehiclesResult as $vehicle)
-                {
-                    $vehicles = $vehicle;
-                }
-
                 $response['vehicles'] = $vehicles;
             }
-            else
-            {
-                $response['outcome'] = "error";
-                $response['err-msg'] = "No vehicles found";
-            }
-
-            $vehicles->close();
-        }
-        else
-        {
+        } else {
             $response['outcome'] = "error";
-            $response['err-msg'] = "Could not get vehicles";
+            $response['err-msg'] = "No vehicles found";
         }
-
-        $stmt->close();
+    } else {
+        $response['outcome'] = "error";
+        $response['err-msg'] = "Could not get vehicles";
     }
+    $stmt->close();
+}
 
-    $connection->close();
-}
-else
-{
-    $response['outcome'] = "error";
-    $response['err-msg'] = "Not authorized";
-}
+$connection->close();
 
 echo json_encode($response);
 
